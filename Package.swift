d41627bd4de25475cc82b5d69200aa5411d406aa
David Young <davidy@passivelogic.com>
2023-10-31 12:33:19 -0400

    Finished

--- Package.swift
+++ Package.swift
@@ -1,12 +1,9 @@
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CLibpl",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CLibpl",
             targets: ["CLibpl"]),
@@ -14,8 +11,6 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CLibpl",
             dependencies: []),
---
+++ umbrella_header.h
@@ -0,0 +1,2 @@
+
+#include </usr/include/plogic/libpl.h>
